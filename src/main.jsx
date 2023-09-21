74ca5576bda01a3a389885244a9c7c9121a2fe6c
greeenOrange <gm1sadiq@gmail.com>
2023-09-21 15:37:44 +0600

    themeadd

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import routers from './routers/routers'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import routers from './routers/routers';
+import { ThemeProvider } from '@mui/material/styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider  theme={theme}>
      <RouterProvider router={routers} />
-  </React.StrictMode>,
+     </ThemeProvider>
+  </React.StrictMode>
 )
